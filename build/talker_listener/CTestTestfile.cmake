c6b5d00735332e7d7fdfb3305eb49e5f60de0830
nishimori <ryu2003ryu@icloud.com>
2024-10-07 15:41:29 +0900

    first sommit

---
+++ build/talker_listener/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/module-dev/ros2_ws/src/talker_listener
+# Build directory: /home/module-dev/ros2_ws/build/talker_listener
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ cc_internals/aarch64-ubuntu-humble/install_rosdeps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -euxo pipefail
+#[apt] Installation commands:
+apt-get install -y ros-humble-ament-cmake ros-humble-rclcpp ros-humble-std-msgs ros-humble-ament-lint-auto ros-humble-ament-lint-common 
