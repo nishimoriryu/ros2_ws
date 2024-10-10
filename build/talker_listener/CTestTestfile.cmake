fa782f0ec0f0ecf7c99924a908084a5f9cc5cc4d
nishimoriryu <ryu2003ryu@icloud.com>
2024-10-10 19:21:13 +0900

    second

--- build/talker_listener/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /ros2_ws/src/talker_listener
-# Build directory: /ros2_ws/build/talker_listener
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
